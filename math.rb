3b3f83b1929dfbe00c50307d158cb875e94feced
Jon Wenger <jonwenger783@gmail.com>
2018-06-05 22:42:06 +0000

    automatically backed up by learn

---
+++ math.rb
@@ -0,0 +1,42 @@
+ 2 + 2
+ 
+ # return four -> addition
+ 
+ 4 * 4
+ 
+ # return 16 -> multiplication
+ 
+ number = 7.0
+ 
+ 0.5 * number
+ 
+ 
+ 
+if 5 = 0 
+  "i'm not a sound math equation"
+  elsif 5 > 3
+  ""
+else
+  "hi"
+end
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
